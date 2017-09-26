0a725684330cc21919055845f2f4aabe1ece8cba
Reunan Le Noc <reunan.lenoc@documalis.com>
2017-09-26 13:13:56 +0200

    (WIP) Component create player

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,5 +1,8 @@
-import { Routes } from '@angular/router';
+import { CreateContainerComponent } from './container/create-container/create-container.component';
 import { LoginContainerComponent } from './container/login-container/login-container.component';
+import { Routes } from '@angular/router';
+
 export const ROUTES: Routes = [
   { path: '', component: LoginContainerComponent },
+  { path: 'create', component: CreateContainerComponent },
 ];
