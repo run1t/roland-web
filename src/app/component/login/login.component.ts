fa82b9c111d47b14b722d00c003e5a8e974e0781
Vincent <crombez.vincent44@gmail.com>
2017-09-26 16:43:05 +0200

    authentication ok in testing mode

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, VERSION } from '@angular/core';
+import { AuthenticationService } from '../../service/authentication.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -7,26 +9,33 @@ import { Component, OnInit, VERSION } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authenticationService:AuthenticationService,
+    private router:Router
+  ) { }
 
   ngOnInit() {
-    console.log(VERSION.full)
+    console.log(VERSION.full);
   }
   
   logger(value:Login){
-    let login:Login;
-    let result:Boolean = false;
+    // const login: Login;
+    // let result: Boolean = false;
 
-    if (value.login === 'Vincent' &&  value.password === 'azerty') {
-      result = true;
-    }
+    // if (value.login === 'Vincent' &&  value.password === 'azerty') {
+    //   result = true;
+    // }
 
-    return result;
+    // return result;
+
+    this.authenticationService.login(value.login, value.password).subscribe(() => {
+      this.router.navigate(['/create']);
+    });
   }
 
 }
 
 interface Login{
-  login: String;
-  password: String;
+  login: string;
+  password: string;
 }
--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class AuthenticationService {
 
   BASE:string = 'http://localhost:3000'
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   login(login:string, password:string){
-    return this.http.post(this.BASE + '/token', JSON.stringify({login: login, password: password}))
-    .map((response:Response) => {
-      let user = response.json();
+    //return this.http.post(this.BASE + '/token', JSON.stringify({login: login, password: password}))
+    return Observable.of({token:'fzefefzf'})
+    .map((user) => {
+      console.log(user);
       if(user && user.token){
         localStorage.setItem('currentUser', JSON.stringify(user));
 
