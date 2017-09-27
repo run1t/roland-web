a5e7fa2332c0a7e40382601321e554a3ae2da00a
Reunan Le Noc <reunan.lenoc@documalis.com>
2017-09-27 15:39:40 +0200

    (build)

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class RolandGarrosPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-header')).getCssValue('color');
   }
 }
